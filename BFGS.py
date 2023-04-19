52d709688794fc290ca99bfbd4f5eebe741f8a0e
Johannes Kjaer <77483678+johannes-kjaer@users.noreply.github.com>
2023-04-19 15:56:58 +0200

    Removed the original P5.py file, which is redundant after separating the code into three different files

--- BFGS.py
+++ BFGS.py
@@ -85,8 +85,89 @@ def StrongWolfe(f,x,p,
     # computations anyway)
     return alphaR #next_x,next_value,next_grad
 
-def lineSearch():
-    pass
+def stepLength(f, X_k, p_k, initAlpha = 1.0, c1 = 1e-2, c2 = 0.9, maxExtItr=500, maxBisItr=500):
+    '''
+    Function for calculating the step length alpha_k, provided a current X_k and a search_direction p_k.
+    Using extrapolation and bisectioning to find a step length satisfying the strong Wolfe conditions.
+    Input:
+    f: The objective function to minimize
+    X_k: The current iterate candidate solution
+    p_k: The search direction to calculate a step in
+    Optional:
+    initAlpha: The step length to first test if is satisfying the strong Wolfe conditions
+    c1: A constant pertaining to the Aramijo condition, satisfying 0 < c1 < 1
+    c2: A constant pertaining to the curvature condition, satisfying c1 < c2 < 1
+    Output:
+    alpha_k: A step length along p_k satisfying the strong Wolfe conditions
+    '''
+
+    multiplier = 2.0            # If the interval is to small, it will be expanded by mulitplying the upper bound with this
+    alphaUpper = initAlpha      # Initial upper bound for alpha, set equal to the default step length
+    alphaLower = 0              # Lower bound for alpha
+    X_k1 = X_k + alphaUpper*p_k # Precomputing the candidate step
+    val_k = f.getVal(X_k)       # Precomputing the value at X_k
+    grad_k =f.getGrad(X_k)      # Precomputing the current gradient
+    grad_k1 = f.getGrad(X_k1)   # Precomputing the gradient at the candidate step
+
+    initDescent = np.inner(p_k,grad_k) # Computing the initial descent along p_k
+    descent_k = np.inner(p_k,grad_k1)
+
+    #print(grad_k)
+    ### Precomputing the Strong Wolfe conditions as boolean values ###
+    #print(f.getVal(X_k1),val_k+c1*alphaUpper*np.inner(grad_k,p_k))
+    armijo = (f.getVal(X_k1) <= val_k+c1*alphaUpper*initDescent)
+    curvatureLow = (descent_k >= c2*initDescent)
+    curvatureHigh = (descent_k <= -c2*initDescent)
+    #print(armijo,curvatureHigh,curvatureLow)
+
+    ##### Finding an interval whose upper end satisfies the armijo condition and the curvatureLow condition, which means the interval is long enough #####
+    iterations = 0
+    while iterations<maxExtItr and (armijo and (not curvatureLow)):
+        alphaLower = alphaUpper  # If the interval is to short, then we set the new lower bound to be the former upper bound
+        alphaUpper *= multiplier # Expanding the interval to hopefully now be able to find a satisfactory alpha_k
+
+        ### Updating values at X_k1 ###
+        X_k1 = X_k + alphaUpper*p_k # Computing the new candidate step
+        grad_k1 = f.getGrad(X_k1)   # Computing the gradient at the new candidate step
+
+        ### Recomputing the strong wolfe conditions for our new interval upper bound ###
+        armijo = f.getVal(X_k1) <= val_k+c1*alphaUpper*np.dot(grad_k,p_k)
+        curvatureLow = np.inner(p_k,grad_k1) >= c2*initDescent
+        curvatureHigh = np.inner(p_k,grad_k1) <= -c2*initDescent
+        #print(armijo,curvatureHigh,curvatureLow)
+
+        iterations += 1
+    
+    if iterations == maxExtItr:
+        print(f'After {iterations} iterations, the interval was still to small to find a suitable alpha_k.')
+
+    #### Using the bisection method to find an alpha_k satisfying the strong Wolfe conditions #####
+    alpha_k = alphaUpper # alpha_k is to be our solution, knowing alphaUpper satisfies two of the three conditions, the first alpha_k candidate is set to be alphaUpper
+
+    iterations = 0 #           and (not (Armijo and curvatureLow and curvatureHigh))
+    while iterations<maxBisItr and (not (armijo and curvatureHigh and curvatureLow)):
+        if armijo and (not curvatureLow):   # Checking if the candidate step length is to small
+            alphaLower = alpha_k            # Increasing the lower bound
+        else:
+            alphaUpper = alpha_k    # If the candidate step length is to large, the upper bound is decreased
+        alpha_k = (alphaUpper-alphaLower)/2 # Updating the candidate step length so that it is in the middle of the interval
+
+        ### Updating values at X_k1 ###
+        X_k1 = X_k + alpha_k*p_k # Computing the new candidate step
+        grad_k1 = f.getGrad(X_k1)   # Computing the gradient at the new candidate step
+
+        ### Recomputing the strong wolfe conditions for our new interval upper bound ###
+        armijo = f.getVal(X_k1) <= val_k+c1*alphaUpper*np.dot(grad_k,p_k)
+        curvatureLow = np.inner(p_k,grad_k1) >= c2*initDescent
+        curvatureHigh = np.inner(p_k,grad_k1) <= -c2*initDescent
+        #print(armijo,curvatureHigh,curvatureLow)
+
+        iterations += 1
+    
+    if iterations == maxBisItr:
+        print(f'After {iterations} iterations, bisectioning the interval still yielded no step length satisfying the strong Wolfe conditions.')
+
+    return alpha_k
 
 def BFGS(f,X_0,
          tol=1e-12, maxItr=20):
--- main.py
+++ main.py
@@ -6,4 +6,6 @@ def main():
     X_0 = np.zeros(4*3)
     print(BFGS.BFGS(objectiveFunctions.P5,X_0))
 
+    #print(BFGS(tesF,np.array([0,0,0])))
+
 main()

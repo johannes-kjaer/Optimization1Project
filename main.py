Updated the expression for the gradient, which now yields the correct values for the analytical X*

main.py
@@ -3,10 +3,12 @@ import BFGS
 import objectiveFunctions
 
 def main():
+    ### Testing that the function is yielding the correct values at the analytical solution ###
     objectiveFunctions.testFunction()
-    #X_0 = np.zeros(4*3)
-    #print(BFGS.BFGS(objectiveFunctions.P5,X_0))
 
-    #print(BFGS(tesF,np.array([0,0,0])))
+    print(BFGS.BFGS(objectiveFunctions.testOF,np.array([0,0,0])))
+    
+    X_0 = np.zeros(4*3)
+    #print(BFGS.BFGS(objectiveFunctions.P5,X_0))
 
 main()

objectiveFunctions.py
@@ -63,37 +63,23 @@ def P5grad(X,k,fixedNodes,edges,extWeights):
         allNodes[X.size:] = X           # and the fixed nodes
         allNodes[:X.size] = fixedNodes  # into one array
 
-        force_cables = np.zeros(X.size)
+        force_cables = np.zeros(X.size) # The gradient of potential energy is a force
         for i in range(M,N):
+            subGradientk = np.zeros(3)  # For each node k (i) the gradient may be calculated separately by calculating the force on the node
             for j in range(N):
-                l_ij = edges[i][j]
-                if l_ij > 0:
+                l_ij = edges[i][j]  # Extracting the resting length of the cable between node i and j
+                if l_ij > 0:        # If there is a cable between the nodes, then proceed
                     norm_ij = np.linalg.norm(allNodes[i*3:i*3+3] - allNodes[j*3:j*3+3])
-                    subGradientk = 0
                     if norm_ij > l_ij:
-                        subGradientk += (k/l_ij**2) *(norm_ij-l_ij) * (allNodes[i*3:i*3+3]-allNodes[j*3:j*3+3])
-                    else:
-                        subGradientk += 0
-                    force_cables[i-M:i-M+3] = subGradientk
+                        forceContribkj = (allNodes[i*3:i*3+3]-allNodes[j*3:j*3+3]) *(1-l_ij/norm_ij) *k/l_ij**2
+                        subGradientk += forceContribkj
+            force_cables[3*(i-M):3*(i-M+1)] = subGradientk
         return force_cables 
-
-
-
-
-        #for i in range(N-M):        # For each free node
-        #    for j in range(i+1,N):      # Go through all of the nodes, calculating the force between i'th and j'th node
-        #        l_ij = edges[i][j]  # Extracting the resting length of the cable
-        #        if l_ij > 0:        # Only proceed if there is a cable between the nodes
-        #            norm_ij = np.linalg.norm(allNodes[3*i:3*i+3]-allNodes[3*j:3*j+3])   # Compute the length between the nodes
-        #            print(f'i:{i}, j:{j}, norm_ij:{norm_ij}, l_ij:{l_ij}, ||{allNodes[3*i:3*i+3]}-{allNodes[3*j:3*j+3]}||, Farr:{force_cables[3*i:3*i+3]}')
-        #            if norm_ij > l_ij:      # Only continue if the cable is stretched, and is creating a force
-        #                force_cables[3*i:3*i+3] += ((norm_ij-l_ij)/l_ij**2) * (allNodes[3*i:3*i+3]-allNodes[3*j:3*j+3]) # Adding the force contribution on the node i from the node j
-        #return force_cables * k     # Return the pull on each node in each direction
 
     def P5weights(extWeights):
         gravitationalPull = np.zeros((3,extWeights.size))   # Creating an array for the gravitational pull on each node, in each direction (all except the z direction will be zero)
         gravitationalPull[2] += extWeights  # Extracting the z coordinate for each node and adding the gravitational pull
-        return gravitationalPull.reshape(3*extWeights.size) # Returning the gravitational pull array
+        return (gravitationalPull.T).reshape(3*extWeights.size) # Returning the gravitational pull array
     
     return P5cables(X,k,fixedNodes,edges) + P5weights(extWeights)
 def P5edges():
@@ -138,12 +124,13 @@ def P5fixedNodes():
 P5 = objectiveFunction(3,P5edges(),P5weights(),P5fixedNodes(),P5val,P5grad) # Gathering the different stationary parts of our problem into one objective function
 ########## --------- ##########
 
-
+########## Test function ##########
 def testFunction():
-    print(P5edges())
     X_star = np.array([2, 2, -3/2,-2, 2, -3/2,-2, -2, -3/2,2, -2, -3/2]) # The analytical solution to the problem.
-    print(f'E_P5={P5.getVal(X_star)}=={7/6}')  # The value of the function at X^* should be about 7/6
-    print(P5.getGrad(X_star)) # The gradient should equal zero at X^*
+    print(f'----------\nE_P5={P5.getVal(X_star)}=={7/6}')  # The value of the function at X^* should be about 7/6
+    print(P5.getGrad(X_star),'\n----------') # The gradient should equal zero at X^*
 
 
-testFunction()
+#testFunction()
+
+testOF = objectiveFunction(3,np.array([[0,1],[1,0]]),np.array([1]),np.array([0,0,3]),P5val,P5grad)